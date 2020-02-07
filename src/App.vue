347f0e97726fa2606b0b8a8f0ad29e77e9ed5ae1
Lorenzo Carlesso <lorcar.job@gmail.com>
2020-02-07 15:11:03 +0100

    implementation new skill

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <News />
     <Lifetime />
     <Skills />
+    <Skill2 />
     <Info />
     <Footer />
   </div>
@@ -16,6 +17,7 @@ import Home from "./pages/Home.vue";
 import News from "./pages/News.vue";
 import Lifetime from "./pages/Lifetime.vue";
 import Skills from "./pages/Skills.vue";
+import Skill2 from "./pages/Skill2.vue";
 import Info from "./pages/Info.vue";
 import Footer from "./pages/Footer.vue";
 
@@ -27,6 +29,7 @@ export default {
     News,
     Lifetime,
     Skills,
+    Skill2,
     Info,
     Footer
   }
---
+++ src/pages/Skill2.vue
@@ -0,0 +1,39 @@
+<template>
+  <section id="skills" class="section">
+    <h1 class="title has-text-centered">Skills 2</h1>
+
+    <div class="container content">
+      <div class="columns has-text-centered">
+        <div
+          class="column is-4"
+          v-for="group in skillGroups"
+          :id="`${group.title}-tab`"
+          :key="`${group.title}-tab`"
+        >
+          <h4 class="title is-4">{{ group.title }}</h4>
+          <ul>
+            <li
+              v-for="(skill, index) in group.skills"
+              :key="`${group.title}_${index}`"
+            >{{ skill.title }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import skillJson from "../assets/json/skill.json";
+
+export default {
+  data() {
+    return {
+      skillGroups: skillJson
+    };
+  }
+};
+</script>
+
+<style>
+</style>
