6e182cd127177ad51afd0fdb0f33136e03ab5d3c
Lorenzo Carlesso <lorcar.job@gmail.com>
2020-04-01 11:38:33 +0200

    trasnlations

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -4,16 +4,16 @@
       <h1 class="title has-text-centered">Info</h1>
       <div class="columns">
         <div class="column is-4">
-          <h6 class="title is-6">Why this site?</h6>
-          <p>My intention is to give a friendly perspective about me.</p>
+          <h6 class="title is-6">{{ $t('info.whyThisSite.title') }}</h6>
+          <p>{{ $t('info.whyThisSite.description') }}</p>
         </div>
         <div class="column is-4">
-          <h6 class="title is-6">Training</h6>
-          <p>I've found good to build something useful to me and to make practice with some new technology.</p>
+          <h6 class="title is-6">{{ $t('info.training.title') }}</h6>
+          <p>{{ $t('info.training.description') }}</p>
         </div>
         <div class="column is-4">
-          <h6 class="title is-6">Warning</h6>
-          <p>I forgot: I am not a web designer but I've tried to expose my contents in a pleasent way.</p>
+          <h6 class="title is-6">{{ $t('info.warning.title') }}</h6>
+          <p>{{ $t('info.warning.description') }}</p>
         </div>
       </div>
     </div>
